306b262cbbc5ede074a95fcad07aa64f7e1700ec
JulianLipa <lipajulianp@gmail.com>
2024-10-08 13:57:42 -0300

    commit 2

--- src/app/components/PropertyCard/PropertyCard.js
+++ src/app/components/PropertyCard/PropertyCard.js
@@ -10,23 +10,6 @@ import Link from "next/link";
 const PropertyCard = ({ data, loading }) => {
   const { deptos, deptosLength, setDeptos } = useAppContext();
 
-  const cartAdd = (e) => {
-    e.preventDefault();
-
-    const existingItem = deptos.find((depto) => depto.title === data.title);
-
-    if (existingItem) {
-      const updatedDeptos = deptos.map((depto) =>
-        depto.title === data.title
-          ? { ...depto, quantity: depto.quantity + 1 }
-          : { ...depto }
-      );
-      setDeptos(updatedDeptos);
-    } else {
-      setDeptos([...deptos, { title: data.title, quantity: 1, data }]);
-    }
-  };
-
   return (
     <div>
       {!loading && <h2 >{data.title}</h2>}
